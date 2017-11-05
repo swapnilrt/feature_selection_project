d849096dca9e64cedfd455b73727fcd27f4c5aca
swapnilrt <swapnil13488@gmail.com>
2017-11-05 14:00:54 +0000

    Done

--- q04_select_from_model/build.py
+++ q04_select_from_model/build.py
@@ -8,3 +8,14 @@ data = pd.read_csv('data/house_prices_multivariate.csv')
 
 
 # Your solution code here
+def select_from_model(df):
+    model = RandomForestClassifier()
+    X, y = df.iloc[:,:-1], df.iloc[:,-1]
+    m = SelectFromModel(model)
+    m.fit(X,y)
+    list_name = [];
+    for i,rank in enumerate(m.get_support()):
+        if(rank==True):
+            list_name.append(X.columns[i])
+
+    return list_name
